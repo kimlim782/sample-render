bb3b37ff0e5fe752e159614879b1c534a92b0faf
kimhun <64160189@go.buu.ac.th>
2025-05-16 16:24:24 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+let express = require('express');
+let app = express();
+
+let port = 3000;
+
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => {
+    res.render('home')
+})
+app.get('/about', (req, res) => {
+    res.render('about')
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
